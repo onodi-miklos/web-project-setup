e87a4658969a020e076e5d0b26b021653acbdedd
Miki <mikonodi33@gmail.com>
2026-02-01 12:51:23 +0100

    Fixed bugs
    Added 'start:dev' script and '@types/express' dependency.

--- setup.js
+++ setup.js
@@ -1,66 +1,77 @@
-const {writeFileSync, mkdirSync} = require('fs');
+const { writeFileSync, mkdirSync } = require("fs");
 
-function setup(path, project){
-  mkdirSync(`${path}/${project}`, { recursive: true })
-  mkdirSync(`${path}/${project}/java`, { recursive: true })
-  mkdirSync(`${path}/${project}/backend`, { recursive: true })
-  mkdirSync(`${path}/${project}/backend/public`, { recursive: true })
-  mkdirSync(`${path}/${project}/backend/dist`, { recursive: true })
-  mkdirSync(`${path}/${project}/backend/src`, { recursive: true })
-  mkdirSync(`${path}/${project}/backend/routes`, { recursive: true })
-  mkdirSync(`${path}/${project}/backend/controllers`, { recursive: true })
-  mkdirSync(`${path}/${project}/backend/frontend`, { recursive: true })
-  mkdirSync(`${path}/${project}/backend/frontend/dist`, { recursive: true })
-  mkdirSync(`${path}/${project}/backend/frontend/src`, { recursive: true })
-  
+function setup(path, project) {
+  mkdirSync(`${path}/${project}`, { recursive: true });
+  mkdirSync(`${path}/${project}/java`, { recursive: true });
+  mkdirSync(`${path}/${project}/backend`, { recursive: true });
+  mkdirSync(`${path}/${project}/backend/public`, { recursive: true });
+  mkdirSync(`${path}/${project}/backend/dist`, { recursive: true });
+  mkdirSync(`${path}/${project}/backend/src`, { recursive: true });
+  mkdirSync(`${path}/${project}/backend/routes`, { recursive: true });
+  mkdirSync(`${path}/${project}/backend/controllers`, { recursive: true });
+  mkdirSync(`${path}/${project}/backend/frontend`, { recursive: true });
+  mkdirSync(`${path}/${project}/backend/frontend/dist`, { recursive: true });
+  mkdirSync(`${path}/${project}/backend/frontend/src`, { recursive: true });
 
-  writeFileSync(`${path}/${project}/README.md`, `
+  writeFileSync(
+    `${path}/${project}/README.md`,
+    `
     npm install
-    `)
-  writeFileSync(`${path}/${project}/package.json`, 
+    `,
+  );
+  writeFileSync(
+    `${path}/${project}/package.json`,
     `{
       "name": "empty",
       "version": "1.0.0",
       "description": "",
       "main": "index.js",
-      "type": "module",
       "scripts": {
             "build:backend": "tsc -p backend",
             "build:frontend": "tsc -p backend/frontend",
             "build": "npm run build:backend && npm run build:frontend",
-            "start": "nodemon backend/dist/app.js"
+            "start": "nodemon backend/dist/app.js",
+            "start:dev": "nodemon --watch 'backend/src' --exec \"npm run build:backend && node backend/dist/app.js\""
       },
       "keywords": [],
       "author": "",
       "license": "ISC",
       "devDependencies": {
             "@types/node": "^25.0.3",
+            "@types/express": "^4.17.21",
             "nodemon": "^3.1.11",
             "typescript": "^5.9.3"
       },
       "dependencies": {
-            "axios": "^1.13.4",
-            "bcrypt": "^6.0.0",
-            "bootstrap": "^5.3.8",
-            "dotenv": "^17.2.3",
-            "express": "^5.2.1",
-            "jsonwebtoken": "^9.0.3",
-            "mongoose": "^9.0.2",
-            "morgan": "^1.10.0",
-            "mysql2": "^3.16.0",
-            "tailwind": "^4.0.0"
-      }
+        "axios": "^1.13.4",
+        "bcrypt": "^6.0.0",
+        "bootstrap": "^5.3.8",
+        "dotenv": "^17.2.3",
+        "express": "^5.2.1",
+        "jsonwebtoken": "^9.0.3",
+        "mongoose": "^9.0.2",
+        "morgan": "^1.10.0",
+        "mysql2": "^3.16.0",
+        "tailwind": "^4.0.0"
+  }
 } 
-    `)
-    
-  writeFileSync(`${path}/${project}/.gitignore`, `
+    `,
+  );
+
+  writeFileSync(
+    `${path}/${project}/.gitignore`,
+    `
     node_modules
     dist
     .env
     log.txt
-    `)
-  writeFileSync(`${path}/${project}/backend/src/app.ts`, `
+    `,
+  );
+  writeFileSync(
+    `${path}/${project}/backend/src/app.ts`,
+    `
     const express = require('express')
+    const path = require('path')
     const app = express()
 
     app.use([express.json(), express.urlencoded({ extended: true }), express.static(path.join(__dirname, './public'))])
@@ -69,16 +80,19 @@ function setup(path, project){
     app.listen(5000, () => {
     console.log('Server listening on port 5000....')
     })
-    `)
-  writeFileSync(`${path}/${project}/backend/tsconfig.json`, `
+    `,
+  );
+  writeFileSync(
+    `${path}/${project}/backend/tsconfig.json`,
+    `
     // BACKEND
     {
   "compilerOptions": {
     "rootDir": "./src",
     "outDir": "./dist",
 
-    "module": "NodeNext",
-    "moduleResolution": "NodeNext",
+    "module": "CommonJS",
+    "moduleResolution": "Node",
     "target": "ES2020",
 
     "lib": ["ES2020"],
@@ -103,8 +117,11 @@ function setup(path, project){
   },
   "include": ["src/**/*"]
 }
-    `)
-  writeFileSync(`${path}/${project}/backend/frontend/tsconfig.json`, `
+    `,
+  );
+  writeFileSync(
+    `${path}/${project}/backend/frontend/tsconfig.json`,
+    `
     // FRONTEND
 {
   "compilerOptions": {
@@ -134,15 +151,21 @@ function setup(path, project){
   },
   "include": ["src/**/*"]
 }
-    `)
-  writeFileSync(`${path}/${project}/backend/frontend/style.css`, `
+    `,
+  );
+  writeFileSync(
+    `${path}/${project}/backend/frontend/style.css`,
+    `
 :root{}
 body{
   margin: none;
   box-sizing: border-box
 }
-    `)
-  writeFileSync(`${path}/${project}/backend/frontend/index.html`, `
+    `,
+  );
+  writeFileSync(
+    `${path}/${project}/backend/frontend/index.html`,
+    `
     <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -156,27 +179,21 @@ body{
     
 </body>
 </html>
-    `)
-  writeFileSync(`${path}/${project}/backend/frontend/src/index.ts`, ``)
+    `,
+  );
+  writeFileSync(`${path}/${project}/backend/frontend/src/index.ts`, ``);
 }
 
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question('loc: ', (loc) => {
-  rl.question('name: ', (name) => {
+rl.question("loc: ", (loc) => {
+  rl.question("name: ", (name) => {
     setup(loc.trim(), name.trim());
     rl.close();
   });
 });
-
-
-
-
-
-
-
